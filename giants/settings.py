8c26be549d91cd2e57ae2f104396bc16c09bc712
Anton Pirker <anton@formunauts.at>
2015-09-17 17:56:55 +0200

    Turned debug mode on again.

--- giants/settings.py
+++ giants/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm3xjd%!_b%_%3s@zie8bu5l8e&avyqc^6q8ace$y*a2kb0ygcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost:8000',
