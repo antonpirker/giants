323c5c547159f834ce0f223305c4123549e364f2
Anton Pirker <anton@ignaz.at>
2015-09-18 16:02:56 +0200

    Added links to rss feed.

--- giants/feeds.py
+++ giants/feeds.py
@@ -14,7 +14,6 @@ class PersonFeed(Feed):
     description = 'We all stand on the shoulders of giants. Here are the giants of IT. One each day.'
 
     def items(self):
-        import ipdb; ipdb.set_trace()
         time_online = datetime.datetime.now().date() - settings.SITE_START_DATE
         return Person.objects.filter(display_order__lte=time_online.days).order_by('-display_order')
 
@@ -24,6 +23,5 @@ class PersonFeed(Feed):
     def item_description(self, item):
         return item.description
 
-    # item_link is only needed if NewsItem has no get_absolute_url method.
     def item_link(self, item):
         return item.get_full_url()
