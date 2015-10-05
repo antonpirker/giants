add8bfbdb1ee887a0003d82712087eaba142138b
Anton Pirker <anton@ignaz.at>
2015-10-05 14:58:47 +0200

    Added script for running files in virtualenv (for supervisord)

---
+++ scripts/run_in_virtualenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VENV=$1
+
+if [ -z $VENV ]; then
+    echo "usage: run_in_virtualenv.sh [virtualenv_path] CMDS"
+    exit 1
+fi
+
+. ${VENV}/bin/activate
+
+shift 1
+echo "Executing $@ in ${VENV}"
+exec "$@"
+
+deactivate
