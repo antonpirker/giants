39cb423e5fe3ddd1f35ca8705f0ba471368353c6
Anton Pirker <anton@ignaz.at>
2015-10-05 10:53:16 +0200

    Cleanup of urls.py

--- giants/urls.py
+++ giants/urls.py
@@ -1,20 +1,5 @@
 # encoding=utf-8
 
-"""giants URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
@@ -27,13 +12,12 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', views.home, name='home'),
-    #url(r'^(?P<month>[0-9]{2})-(?P<day>[0-9]{2})$', views.person, name='person-short-url'),
     url(r'^(?P<display_order>[0-9]+)$', views.person, name='person-short-url'),
     url(r'^(?P<display_order>[0-9]+)/(?P<name>[\w-]+)$', views.person, name='person'),
 
     url(r'^feed$', PersonFeed()),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
