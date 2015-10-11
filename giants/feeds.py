704e95eabab96aaf01cf1a27eda665c1c68c5bf2
Anton Pirker <anton@ignaz.at>
2015-10-11 20:35:24 +0200

    Small update to rss feed

--- giants/feeds.py
+++ giants/feeds.py
@@ -4,24 +4,42 @@ import datetime
 
 from django.conf import settings
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
 
 from .models import Person
 
 
 class PersonFeed(Feed):
-    title = settings.SITE_NAME
     link = "/feed"
-    description = 'We all stand on the shoulders of giants. Here are the giants of IT. One each day.'
+    feed_type = Atom1Feed
+    title = settings.SITE_NAME
+    subtitle = 'We all stand on the shoulders of giants. Here are the giants of IT. One each day.'
 
     def items(self):
         time_online = datetime.datetime.now().date() - settings.SITE_START_DATE
         return Person.objects.filter(display_order__lte=time_online.days).order_by('-display_order')
 
+    def author_email(self, item):
+        return 'anton@ignaz.at'
+
+    def author_name(self, item):
+        return 'Anton Pirker'
+
     def item_title(self, item):
         return item.name
 
     def item_description(self, item):
-        return item.description
+        desc = '<![CDATA[%(text)s\n\n<a href="%(link)s">%(link)s</a>]]>' % {
+            'text': item.description,
+            'link': item.wikipedia_link
+        }
+        return desc
 
     def item_link(self, item):
         return item.get_full_url()
+
+    def item_author_email(self, item):
+        return 'anton@ignaz.at'
+
+    def item_author_name(self, item):
+        return 'Anton Pirker'
